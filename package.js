9f85513361b62c992d0522ab15906c8cc1c312d4
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-06-06 22:55:42 +0530

    info moved

--- package.js
+++ package.js
@@ -108,9 +108,3 @@ function configurePackage(api) {
 
   api.add_files(['lib/client/route.js'], 'client')
 }
-
-console.log("---------------------------------------------------------");
-console.log("`apm` package is deprecated!");
-console.log("Please use `kadira` package instead: http://git.io/yoagYw");
-console.log("----------------------------------------------------------");
-
